ec4127593b137e50f56d8ef933c468bd8a3485cc
Avadhesh Sharma <apple.avadhesh@gmail.com>
2021-01-27 13:49:05 +0530

    Update ViewController.swift

--- Jenkins_Test/ViewController.swift
+++ Jenkins_Test/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("Webhook works!!!!!")
+        print("Webhook works!")
     }
 
 
